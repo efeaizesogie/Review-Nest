4e34aea5e4831e5b28bda229ce58c25832281237
Eronini Kelechi <kelechieronini@yahoo.com>
2023-08-16 18:45:56 +0100

    created sign-up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
