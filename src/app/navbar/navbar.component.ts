a7ec6d2dd1035c6a0bc8c658898680c2bb6de6f6
Efe Aizesogie <efeaizesogie@gmail.com>
2023-08-18 07:34:19 +0100

    updated  landing page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  navigateToNewPage() {
+    this.router.navigateByUrl("/sign-up");
+  }
 }
+
+function showSignUp() {}
---
+++ src/app/sign-up-layout/sign-up-layout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-sign-up-layout",
+  template: "<router-outlet></router-outlet>",
+})
+export class SignUpLayoutComponent {}
