7884894b958d76b647caa3ad2778a7dceec276ea
efeaizesogie <efeaizesogie@gmail.com>
2023-08-24 18:23:01 +0100

    review dashboard edited

--- src/app/review-dashboard/review-dashboard.component.ts
+++ src/app/review-dashboard/review-dashboard.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ReviewService } from "../review.service";
 
 @Component({
-  selector: 'app-review-dashboard',
-  templateUrl: './review-dashboard.component.html',
-  styleUrls: ['./review-dashboard.component.css']
+  selector: "app-review-dashboard",
+  templateUrl: "./review-dashboard.component.html",
+  styleUrls: ["./review-dashboard.component.css"],
 })
-export class ReviewDashboardComponent {
+export class ReviewDashboardComponent implements OnInit {
+  reviews: any;
+  totalReviews: any;
 
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+
+  get startIndex(): number {
+    return (this.currentPage - 1) * this.itemsPerPage;
+  }
+
+  get endIndex(): number {
+    return this.startIndex + this.itemsPerPage;
+  }
+
+  constructor(private reviewService: ReviewService) {}
+
+  ngOnInit(): void {
+    this.reviewService.getReviews().subscribe(
+      (response) => {
+        this.reviews = response.Reviews;
+        this.totalReviews = this.reviews.length;
+        console.log(this.reviews);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    const totalPages = Math.ceil(this.totalReviews / this.itemsPerPage);
+    if (this.currentPage < totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  changePage(page: number) {
+    if (page >= 1 && page <= Math.ceil(this.totalReviews / this.itemsPerPage)) {
+      this.currentPage = page;
+    }
+  }
+
+  getPageNumbers(): number[] {
+    const totalPages = Math.ceil(this.totalReviews / this.itemsPerPage);
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
 }
--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ReviewService {
-  constructor(private http: HttpClient) { }
+  private accessToken = localStorage.getItem("accessToken");
+  constructor(private http: HttpClient) {}
 
   submitReview(reviewData: any): Observable<any> {
-    const userID = localStorage.getItem('userID'); // Retrieve user ID here
+    const userID = localStorage.getItem("userID"); // Retrieve user ID here
     const apiUrlWithUserID = `https://reviewnest.onrender.com/api/v1/reviews/create/${userID}`;
-    console.log('hello')
+    console.log("hello");
 
     console.log(this.http.post(apiUrlWithUserID, reviewData));
     return this.http.post(apiUrlWithUserID, reviewData);
+  }
+
+  getReviews(): Observable<any> {
+    const userID = localStorage.getItem("userID");
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.accessToken}`,
+    });
+
+    console.log(
+      this.http.get<any>(
+        `https://reviewnest.onrender.com/api/v1/reviews/${userID}`
+      )
+    );
+    return this.http.get<any>(
+      `https://reviewnest.onrender.com/api/v1/reviews/${userID}`,
+      { headers }
+    );
   }
 }
