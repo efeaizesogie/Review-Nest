2709e12a16942d5b56221dc3d41f517ad01f8632
efeaizesogie <efeaizesogie@gmail.com>
2023-08-30 21:59:58 +0100

    edited recent reviews

--- src/app/recent-review/recent-review.component.ts
+++ src/app/recent-review/recent-review.component.ts
@@ -1,20 +1,35 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import { ReviewService } from "../review.service";
 
 @Component({
   selector: "app-recent-review",
   templateUrl: "./recent-review.component.html",
   styleUrls: ["./recent-review.component.css"],
 })
-export class RecentReviewComponent implements AfterViewInit {
-  @ViewChild("scrollSection") scrollSection!: ElementRef;
+export class RecentReviewComponent implements OnInit {
+  reviews: any;
+  totalReviews: any;
+  loading: boolean = true;
 
-  ngAfterViewInit() {
-    this.scrollSection.nativeElement.addEventListener(
-      "animationiteration",
-      () => {
-        this.scrollSection.nativeElement.appendChild(
-          this.scrollSection.nativeElement.firstElementChild
-        );
+  constructor(
+    private reviewService: ReviewService,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    const loadingScreen = document.querySelector(".spinner-item");
+    this.reviewService.getReviewsWithoutId().subscribe(
+      (response) => {
+        this.reviews = response.Reviews;
+        this.totalReviews = this.reviews.length;
+        console.log(this.reviews);
+        this.loading = false;
+        this.renderer.setStyle(loadingScreen, "display", "none");
+      },
+      (error) => {
+        console.error(error);
+        this.loading = false;
+        this.renderer.setStyle(loadingScreen, "display", "none");
       }
     );
   }
