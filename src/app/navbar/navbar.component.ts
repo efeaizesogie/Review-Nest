67c79edf71d099befcb8c597ceab57ab9e5c3f22
efeaizesogie <efeaizesogie@gmail.com>
2023-08-31 16:49:26 +0100

    edited sign up button visibility

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import {
   EventEmitter,
   Output,
   Renderer2,
+  OnInit,
 } from "@angular/core";
 
 @Component({
@@ -12,8 +13,13 @@ import {
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.css"],
 })
-export class NavbarComponent implements AfterViewInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
+  accessToken: any;
   constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.accessToken = localStorage.getItem("accessToken");
+  }
 
   navigateToSignUp() {
     const anchor = this.renderer.createElement("a");
@@ -28,24 +34,5 @@ export class NavbarComponent implements AfterViewInit {
     }
   }
 
-  ngAfterViewInit(): void {
-    // if (window.innerWidth < 980) {
-    //   this.applyCollapseOnLinkClick();
-    // }
-  }
-
-  // applyCollapseOnLinkClick() {
-  //   const links = document.querySelectorAll(".link-items") as NodeListOf<
-  //     HTMLAnchorElement
-  //   >;
-
-  //   links.forEach((link) => {
-  //     link.addEventListener("click", function () {
-  //       const navbarCollapse = document.querySelector(".navbar-collapse");
-  //       if (navbarCollapse && navbarCollapse.classList.contains("show")) {
-  //         navbarCollapse.classList.remove("show");
-  //       }
-  //     });
-  //   });
-  // }
+  ngAfterViewInit(): void {}
 }
